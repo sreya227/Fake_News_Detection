3454ba5c58d792143af1c36e5b737289313cf79a
sreya227 <118985555+sreya227@users.noreply.github.com>
2025-04-01 02:29:45 +0530

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -1,28 +1,28 @@
-import pickle
-from sklearn.feature_extraction.text import TfidfVectorizer
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  4 17:45:40 2017
 
-# Get user input
+@author: NishitP
+"""
+
+import pickle
+
+#doc_new = ['obama is running for president in 2016']
+
 var = input("Please enter the news text you want to verify: ")
 print("You entered: " + str(var))
 
-# Function for making the prediction
-def detecting_fake_news(var):
-    # Load the model and the vectorizer
-    load_model = pickle.load(open('path_to_final_model/final_model.sav', 'rb'))
-    vectorizer = pickle.load(open('path_to_vectorizer/vectorizer.sav', 'rb'))
-  # Adjust path if necessary
 
-    # Preprocess the input (vectorize it)
-    var_vectorized = vectorizer.transform([var])
+#function to run for prediction
+def detecting_fake_news(var):    
+#retrieving the best model for prediction call
+    load_model = pickle.load(open('final_model.sav', 'rb'))
+    prediction = load_model.predict([var])
+    prob = load_model.predict_proba([var])
 
-    # Predict the class and the probability
-    prediction = load_model.predict(var_vectorized)
-    prob = load_model.predict_proba(var_vectorized)
+    return (print("The given statement is ",prediction[0]),
+        print("The truth probability score is ",prob[0][1]))
 
-    # Output the result
-    print("The given statement is:", prediction[0])
-    print("The truth probability score is:", prob[0][1])
 
-# Run the function
 if __name__ == '__main__':
     detecting_fake_news(var)
